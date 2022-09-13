9277c72e92c2a54de7d3f42efbc8f292de5a58ed
Luke <l.hawk99@hotmail.com>
2022-09-13 00:25:21 +0000

    got further on assignemnt 1

--- t.py
+++ t.py
@@ -2,7 +2,28 @@ import numpy as np
 
 import qiskit as qk
 
+from qiskit import QuantumCircuit, transpile, Aer, IBMQ, QuantumRegister, ClassicalRegister
+import qiskit as qk
+from qiskit.quantum_info.operators import Operator
 
+
+def Q1(a, b):
+    qc = QuantumCircuit(2, 2)
+    qc.initialize([a,b], 0)
+    qc.cx(0,1)
+    qc.s(1)
+    # qc.barrier()
+    # cx = Operator([
+    #     [1, 0],
+    #     [0, 0]
+    # ])
+    # qc.unitary(cx, 0, label='A')
+    qc.measure(0,0)
+    qc.z(1).c_if(0, 0)
+    
+
+    
+    return qc
 
 def foo():
     pass
